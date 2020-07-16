23dec790d589dfe5512020da46289e294fc4ddcd
Abbey List <agl24@duke.edu>
2020-07-16 03:22:43 -0400

    Organized/refined content and layout of Test Statistic/P-Value tab

--- hyptestapp/app.R
+++ hyptestapp/app.R
@@ -145,6 +145,39 @@ MathJax.Hub.Config({
                                            font-size: 17px
                                            } #choose6{
                                            font-size: 17px
+                                           } #tab21 {
+                                           font-size: 17px
+                                           } #tab22 {
+                                           font-size: 17px
+                                           } #tab2test{
+                                           font-size: 17px
+                                           } #tab23 {
+                                           font-size: 17px
+                                           } #tab24{
+                                           font-size: 17px
+                                           } #tab25{
+                                           font-size: 17px
+                                           } #tab2testd {
+                                           font-size: 17px
+                                           } #tab26 {
+                                           font-size: 17px
+                                           } #tab27{
+                                           font-size: 17px
+                                           } #tab28{
+                                           font-size: 17px
+                                           } #tab2a{
+                                           font-size: 17px
+                                           } #tab29 {
+                                           font-size: 23px;
+                                           text-align: center
+                                           } #tab210{
+                                           font-size: 17px
+                                           } #concrules {
+                                           font-size: 15px
+                                           } #tab211{
+                                           font-size: 17px
+                                           } #tab281{
+                                           font-size: 17px
                                            }"
                          )
                       ),
@@ -188,12 +221,34 @@ MathJax.Hub.Config({
                       # Title/subtitle
                       
                       h2("Test Statistic, P-Value, and Alpha", align = "center"),
+                    
+                      
+                      textOutput("tab21"),
+                      br(),
+                      p(textOutput("tab22", inline = TRUE), span(textOutput("tab2test", inline = TRUE), style = "color:cornflowerblue; font-size:17px"), textOutput("tab23", inline = TRUE) ),
+                      helpText("$\\huge{t^{*} = \\frac{\\bar{x}-\\mu_0}{\\frac{s}{\\sqrt{n}}}}$", align = "center"),
+                      
+                      bsPopover(id = "tab2test", 'The test statistic is essentially a summary of the sample data, telling us how far it falls from the hypothesized distribution. The given formula is for a hypothesis test of a mean when the population standard deviation is unknown, but the general format is always (sample statistic minus hypothesized value) divided by the standard error (or population standard deviation if known).', trigger = "hover", placement = "top"),
+                      
+                      uiOutput("tab2testd"),
                       
                       br(),
                       
-                      h4("Test statistic - The test statistic is essentially a summary of your sample data that compares it with what is expected under the null hypothesis. The general form is (sample stat minus null parameter) / (standard deviation of sample stat)."),
+                      p(textOutput("tab24", inline = TRUE), span(textOutput("tab2p", inline = TRUE), style = "color:cornflowerblue; font-size:17px"), uiOutput("tab25", inline = TRUE) ),
+                      
+                      bsPopover(id = "tab2p", 'The p-value is directly related to the test statistic -- it is the probability of observing a test statistic as or more extreme than the one observed, given that the null hypothesis is true. In other words, it is the probability of seeing data with evidence against the null hypothesis that is as strong or stronger that the data you have seen. A test for a mean with unknown population standard deviation uses the t-distribution to find the p-value, but other types of tests may use the z-distribution, f-distribution, etc.', trigger = 'hover', placement = "top"),
+                      
                       br(),
-                      h4("p-value : the p value is related to the test statistic -- it is the probability of observing a test statistic as or more extreme than the observed one, given that the null hypothesis is true. In other words, it is the probability of seeing data with evidence as strong or stronger than the data you have seen. Using the distribution of the test statistic (normal, f-distribution, etc.), you can find the probability associated with observing that t-statistic or more extreme, which gives the p-value."),
+                      
+                      p(textOutput("tab26", inline = TRUE), span(uiOutput("tab261", inline = TRUE), style = "color:cornflowerblue; font-size:17px"), uiOutput("tab2a", inline = TRUE), uiOutput("tab27", inline = TRUE), span(textOutput("tab2c", inline = TRUE), style = "color:cornflowerblue; font-size:17px"), textOutput("tab28", inline = TRUE) ),
+                      
+                      bsPopover(id = "tab261", "The significance level is also the probability of a Type I Error, or rejecting the null hypothesis when it is true. A smaller alpha is often chosen in situations where a Type I Error could be potentially harmful, such as releasing an unsafe drug or costing a company lots of money. With a one-sided test, we compare the p-value to alpha, but with a two-sided test, we compare the p-value to alpha divided by 2, since alpha (a probability) is split between the two tails of the distribution.", trigger = 'hover', placement = 'top'),
+                      
+                      bsPopover(id = "tab2c", "The critical value is found from the t-distribution table, which usually has labels for one-sided and two-sided scenarios. Similar to how the p-value is compared to alpha (or alpha divided by 2, for two-tailed tests), the test statistic can be compared to the critical value to make a conclusion.", trigger = 'hover', placement = 'top'),
+                      
+                      br(),
+                      
+                      textOutput("tab281"),
                       
                       fluidRow(
                         column(width = 9,
@@ -204,7 +259,15 @@ MathJax.Hub.Config({
                                checkboxInput("showpval", "Show p-value", value = TRUE),
                                checkboxInput("showalpha", "Show alpha", value = TRUE)),
                                textOutput("conclusion")
-                      )
+                      ),
+                      
+                      textOutput("tab29"),
+                      br(),
+                      textOutput("tab210"),
+                      br(),
+                      uiOutput("concrules"),
+                      textOutput("tab211"),
+                      br()
                       
                       ),
              
@@ -394,6 +457,28 @@ server <- function(input, output) {
   output$tab14 <- renderText({ c('When we perform the test, it is important to remember that no matter what our conclusions are, we have not "proved" either claim to be indisputably true; we have simply observed a sample dataset that either has strong evidence against the status quo or does not. We will either: ')})
   output$conc <- renderUI(withMathJax(HTML("<ul><li>Reject the null hypothesis, meaning that the evidence was strong enough for us to reject $\\huge{H_0}$ and be in favor of $\\huge{H_A}$, or</li><li>Fail to reject the null hypothesis, meaning that the evidence was not strong enough to support $\\huge{H_A}$ and reject $\\huge{H_0}$.</li></ul>")))
   output$tab15 <- renderText({ c("Neither hypothesis is definitely proved, confirmed, or disproved -- the test will only tell us which conclusion this dataset supports.")})
+  
+  
+  # Text for Test Statistic/P-Value Tab
+  output$tab21 <- renderText({ c("When performing the hypothesis test, the calculations below are done with the assumption that the null distribution is true.")})
+  output$tab22 <- renderText({ c("In a hypothesis test for a mean, the formula for the ")})
+  output$tab2test <- renderText({ c("test statistic")})
+  output$tab23 <- renderText({ c(" takes this general formula:")})
+  output$tab24 <- renderText({ c("To find the ")})
+  output$tab2p <- renderText({ c("p-value")})
+  output$tab25 <- renderUI({ withMathJax(c(", we will need to use the t-distribution with $\\huge{n-1}$ degrees of freedom. Generally, a t-distribution calculator is used for this, although you can estimate ranges for the p-value from the t-table."))})
+  output$tab2testd <- renderUI(withMathJax(c("where $\\huge{\\bar{x}}$ is the sample mean, $\\huge{\\mu_0}$ is the hypothesized value, $\\huge{s}$ is the sample standard deviation, and $\\huge{n}$ is the sample size.")))
+  output$tab26 <- renderText({c("The ")})
+  output$tab261 <- renderText({ c("significance level")})
+  output$tab2a <- renderUI( withMathJax(c(", or alpha ($\\huge{\\alpha}$)")))
+  output$tab27 <- renderUI( withMathJax( c(", is chosen by the researcher before performing the test. It gives the expected proportion of tests that will result in a false positive, or rejecting the null hypothesis when it is true. From $\\huge{\\alpha}$, we can find the ")))
+  output$tab2c <- renderText({ c("critical value")})
+  output$tab28 <- renderText({ c(" from the t-distribution, which is analogous to the test statistic -- it is the point where the probability of seeing a value as or more extreme is equal to the significance level.")})
+  output$tab281 <- renderText({ c("Below is a plot of the null distribution with the test statistic, p-value, and significance level displayed. We can make a conclusion based on our simulation, with parameters from the sidebar.")})
+  output$tab29 <- renderText({ c("Making Conclusions")})
+  output$tab210 <- renderText({ c("To make a conclusion for our hypothesis test, we can either use the p-value and compare it to the significance level, or we can use the test statistic and compare it to the critical value. For any given hypothesis test, both methods will yield the same conclusion. The decision rules are as follows:")})
+  output$concrules <- renderUI(withMathJax(HTML("<ul><li>If the p-value is <b>less than</b> $\\huge{\\alpha}$ or the test statistic is <strong>greater than</strong> the critical value, <strong>reject</strong> $\\huge{H_0}$ in favor of $\\huge{H_A}$.</li><li>If the p-value is <strong>greater than</strong> $\\huge{\\alpha}$ or the test statistic is <strong>less than</strong> the critical value, <strong>fail to reject</strong> $\\huge{H_0}$ in favor of $\\huge{H_A}$.</li></ul>")))
+  output$tab211 <- renderText({ c("For practical interpretations of these conclusions, see the Null and Alternative Hypotheses tab.")})
   
   
   # Hige sidebar on Overview and Review Quiz tabs
